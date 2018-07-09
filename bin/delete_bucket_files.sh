26f771c616e04377465c6bf8b9f6db7e9db8b47f
Kyle <none>
2018-07-08 20:29:21 -0700

    Add helper script for cleaning out s3 bucket prior to removing stack

---
+++ bin/delete_bucket_files.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -eu
+
+STAGE="${1:-dev}"
+
+BUCKET_NAME=$(aws \
+    cloudformation describe-stacks \
+    --stack-name "gladnews-${STAGE}" \
+    --query "Stacks[0].Outputs[?OutputKey=='ScriptBucket'] | [0].OutputValue" \
+    --output text)
+
+echo "You are about to DELETE ALL files for ${STAGE}, bucket ${BUCKET_NAME}"
+echo "Press Ctrl-C to stop, or any key to proceed."
+
+read
+
+echo "Deleting static assets from ${BUCKET_NAME}..."
+
+mkdir /tmp/empty
+
+aws s3 sync --delete /tmp/empty/ "s3://${BUCKET_NAME}/"
+
+rmdir /tmp/empty
+
+echo "Bucket ${BUCKET_NAME} has been emptied"
