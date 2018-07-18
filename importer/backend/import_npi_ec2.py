8a6098e754c7f5bd684c67accc939d4bbe47f169
Kyle <none>
2018-07-17 23:48:52 -0700

    Fix ssm queries

--- importer/backend/import_npi_ec2.py
+++ importer/backend/import_npi_ec2.py
@@ -6,7 +6,7 @@ user_data_tmpl = """#!/bin/bash
 env >> /tmp/myenvironment
 pwd >> /tmp/pwd
 whoami >> /tmp/whoami
-sudo yum install -y awscli python3 python3-devel python36-pip gcc mysql-devel
+sudo yum install -y awscli python3 python3-devel python36-devel python36-pip gcc mysql-devel
 # aws s3 cp s3://{s3_bucket}/lib /opt/ --recursive
 # aws s3 cp s3://{s3_bucket}/npidata_pfile_10k.csv data/
 aws s3 cp s3://{s3_bucket}/importer.tar.gz /opt
@@ -14,9 +14,10 @@ cd /opt
 tar xzvf importer.tar.gz
 cd importer*
 sudo pip-3.6 install -r requirements.txt
-export db_user=$(aws ssm get-parameters --names "db_user" --region us-east-1 --with-decryption)
-export db_password=$(aws ssm get-parameters --names "db_password" --region us-east-1 --with-decryption)
-export db_host=$(aws ssm get-parameters --names "db_host" --region us-east-1 --with-decryption)
+export db_user=$(aws ssm get-parameters --names "db_user" --region us-east-1 --with-decryption --query Parameters[0].Value --output text)
+export db_password=$(aws ssm get-parameters --names "db_password" --region us-east-1 --with-decryption --query Parameters[0].Value --output text)
+export db_host=$(aws ssm get-parameters --names "db_host" --region us-east-1 --with-decryption --query Parameters[0].Value --output text)
+export db_schema=$(aws ssm get-parameters --names "db_schema" --region us-east-1 --with-decryption --query Parameters[0].Value --output text)
 env >> /tmp/myenvironment2
 ./runner-import.py npi create
 """
