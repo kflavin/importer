04eed8b1bf10c0a7fd9998db84e3fe7beb46077b
Kyle <Kyle.Flavin@gmail.com>
2019-07-07 22:24:18 -0700

    Update truncate table SP to only do the rxnorm stage tables

--- importer/sql/products/sp/sp_truncate_stage_tables.sql
+++ importer/sql/products/sp/sp_truncate_stage_tables.sql
@@ -1,13 +1,15 @@
-CREATE DEFINER=`{user}`@`%` PROCEDURE `{database}`.`sp_truncate_stage_tables`()
+CREATE PROCEDURE sp_truncate_stage_tables()
 BEGIN
-	truncate table stage_cms_service;
-    truncate table stage_drugVocabulary;
-    truncate table stage_fda_contacts;
-    truncate table stage_fda_device;
-    truncate table stage_fda_gmdn;
-    truncate table stage_fda_identifiers;
-    truncate table stage_indications;
-    truncate table stage_marketingcodes;
-    truncate table stage_ndc_product;
-    truncate table stage_orangebook_product;
+#     truncate table stage_cms_service;
+#     truncate table stage_drugVocabulary;
+#     truncate table stage_fda_contacts;
+#     truncate table stage_fda_device;
+#     truncate table stage_fda_gmdn;
+#     truncate table stage_fda_identifiers;
+#     truncate table stage_indications;
+#     truncate table stage_marketingcodes;
+#     truncate table stage_ndc_product;
+#     truncate table stage_orangebook_product;
+    truncate table stage_rxnconso;
+    truncate table stage_rxnrel;
 END
