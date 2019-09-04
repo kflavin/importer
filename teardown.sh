b86f22638f7722c846fc2e10d2eacbce627b7292
Kyle <Kyle.Flavin@gmail.com>
2019-09-03 18:56:25 -0700

    Cleanup SSM parameters on teardown

--- teardown.sh
+++ teardown.sh
@@ -6,3 +6,9 @@ echo "Tear down ${STAGE} with .env.${STAGE}"
 
 bin/delete_bucket_files.sh $STAGE
 sls remove --stage=$STAGE
+
+aws ssm delete-parameter --name /importer/${STAGE}/db_host
+aws ssm delete-parameter --name /importer/${STAGE}/db_user
+aws ssm delete-parameter --name /importer/${STAGE}/db_password
+aws ssm delete-parameter --name /importer/${STAGE}/db_schema
+aws ssm delete-parameter --name /importer/${STAGE}/stage_db_schema
