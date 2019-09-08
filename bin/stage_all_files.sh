8d2512cf981bca8dcb3b2f2d731280ac9826a1b8
Kyle <Kyle.Flavin@gmail.com>
2019-09-08 11:02:21 -0700

    Copy latest rxnorm file during deployment

--- bin/stage_all_files.sh
+++ bin/stage_all_files.sh
@@ -15,5 +15,8 @@ aws s3 cp --recursive lambdas/resources/config/ s3://${BUCKET_NAME}/config/
 echo "----- Build runner -----"
 python setup.py sdist
 echo "----- Copy runner to ${BUCKET_NAME} -----"
-latest_file=$(ls -tr dist/*.tar.gz | tail -n 1)
-aws s3 cp $latest_file s3://${BUCKET_NAME}/importer.tar.gz
+latest_runner_file=$(ls -tr dist/*.tar.gz | tail -n 1)
+aws s3 cp $latest_runner_file s3://${BUCKET_NAME}/importer.tar.gz
+
+latest_rxnorm_file=$(ls -tr talend/dist/*.zip | tail -n 1)
+aws s3 cp $latest_rxnorm_file s3://${BUCKET_NAME}/RxNorm_Loader.zip
