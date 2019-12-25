8637144750ec1a97cd832ad18ace0a95fbc8ad6c
Kyle <Kyle.Flavin@gmail.com>
2019-12-24 16:15:29 -0800

    Consolidate NPI lambda into a single function.
    Add most environment variables into serverless.env.yml files.
    
    Add a debug option to the NPI userdata
    
    Make deletion policy a variable

--- lambdas/npi/loader.py
+++ lambdas/npi/loader.py
@@ -1,35 +1,51 @@
 import os
-from importer import monthly_prefix as bucket_prefix
 from lambdas.helpers.db import DBHelper
 from lambdas.helpers.ec2 import EC2Helper
-from lambdas.helpers.file_loader import loader_user_data
-from lambdas.periods import MONTHLY as period
+from lambdas.periods import WEEKLY, MONTHLY
+from importer import weekly_prefix, monthly_prefix
 
+from lambdas.helpers.file_loader import loader_user_data
 user_data_head_tmpl = loader_user_data("start")
 user_data_body_tmpl = loader_user_data("npi/body")
 user_data_finish_tmpl = loader_user_data("finish")
 
 
 def handler(event, context):
-    print(f"Starting {period} import...")
     print(event)
+    period = event.get('period')
+    debug_flag = ""
+    init_flag = ""
 
-    # Run first time to initialize database.  This will zero out deactivated NPI data.
-    initialize = event.get('initialize', False)
-    init_flag = "--initialize" if initialize else ""
-    print(f"Initialize?: {initialize}, init_flag={init_flag}")
+    if not period:
+        raise Exception("Please pass a period ('monthly' or 'weekly')")
+
+    if event.get('debug', False):
+        debug_flag = "--debug"
+
+    if period == MONTHLY:
+        instance_type = os.environ.get('monthly_instance_type')
+        timeout = os.environ.get('timeout_monthly', 150)
+        bucket_prefix = monthly_prefix
+
+        # Run first time to initialize database.  This will zero out deactivated NPI data.
+        init_flag = "--initialize" if event.get('initialize', False) else ""
+    else:
+        instance_type = os.environ.get('weekly_instance_type')
+        timeout = os.environ.get('timeout_weekly', 20)
+        bucket_prefix = weekly_prefix
+
+    print(f"Starting {period} import")
 
     environment = os.environ.get('environment', 'dev')
     region = os.environ.get('aws_region')
     key_name = os.environ.get('aws_key')
     image_id = os.environ.get('aws_image_id')
-    instance_type = os.environ.get('aws_instance_type')
     security_groups = os.environ.get('aws_security_groups').split(",")
-    subnet_id = os.environ.get('aws_subnets').split(",")[0]      # Just use the first subnet
+    subnet_id = os.environ.get('aws_subnets').split(",")[0]   # Just use the first subnet
     instance_profile = os.environ.get('aws_instance_profile')
     table_name = os.environ.get('npi_table_name')
     log_table_name = os.environ.get('npi_log_table_name')
-    timeout = os.environ.get('monthly_import_timeout', '30')
+    max_concurrent_instances = int(os.environ.get('npi_max_instances', 1))
     bucket_name = os.environ.get("aws_s3_bucket")
     sns_topic_arn = os.environ.get("aws_sns_topic_arn")
     terminate_on_completion = os.environ.get("terminate_on_completion")
@@ -37,16 +53,17 @@ def handler(event, context):
     ec2 = EC2Helper(region, period)
     rds = DBHelper(region)
 
-    # filename = bucket_key.split("/")[-1]
-    print(f"bucket: {bucket_name} prefix: {bucket_prefix} table: {table_name} period: {period}")
+    print(f"instance_type: {instance_type}, initialize: {init_flag}, debug: {debug_flag}, bucket: {bucket_name}, "
+          f"prefix: {bucket_prefix}, table: {table_name}")
 
     if not rds.files_ready(log_table_name, period, environment, 1):
         print(f"No files in {bucket_name}/{bucket_prefix} are ready for import.")
         return False
 
     active_imports = ec2.active_imports(table_name, environment)
-    print(f"Current number of tasks are {active_imports}, max instances are 1")
-    if active_imports > 0:
+    print(f"Current number of tasks are {active_imports}, max instances are {max_concurrent_instances}")
+
+    if active_imports >= max_concurrent_instances:
         print(f"SKIPPING, there is already an import running for table {table_name}.")
         return False
 
@@ -65,6 +82,7 @@ def handler(event, context):
                                                 period=period,
                                                 timeout=timeout,
                                                 init_flag=init_flag,
+                                                debug_flag=debug_flag,
                                                 limit=1)
 
     user_data_finish = user_data_finish_tmpl
@@ -73,14 +91,14 @@ def handler(event, context):
 
     # Run the instance
     instance = ec2.run(key_name, image_id, instance_type, subnet_id, user_data, instance_profile, 
-                       security_groups, context.function_name, table_name, environment)
+                        security_groups, context.function_name, table_name, environment)
 
     print(f"Instance: {instance}")
     return True
 
-    
-# For testing from the cli
+
 if __name__ == '__main__':
+    print("Testing import from the CLI")
     class Object(object):
         pass
 
