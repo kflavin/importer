b562f70b8d37afb75aed27894e33ccbb452c460c
Kyle <Kyle.Flavin@gmail.com>
2019-12-28 15:04:17 -0800

    Bump version: 0.12.3 → 0.12.4

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ class CleanCommand(distutils.cmd.Command):
 
 
 setup(name='importer',
-      version='0.12.3',
+      version='0.12.4',
       description='Importer',
       url="https://github.com/rxvantage/data-importer",
       maintainer="Kyle F",
