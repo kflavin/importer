514890164e0cdb3720f4c6a481889da55cd1fe90
Kyle <Kyle.Flavin@gmail.com>
2019-12-28 13:35:05 -0800

    Add clean command to setup.py
    Fix version

--- setup.py
+++ setup.py
@@ -1,19 +1,43 @@
 from setuptools import setup, find_packages
+import distutils
+import shutil
+import glob
+import os
  
-# setup(
-#     name='importer',    # This is the name of your PyPI-package.
-#     version='0.1',                          # Update the version number for new releases
-#     scripts=['runner-import']                  # The name of your scipt, and also the command you'll be using for calling it
-# )
+
+class CleanCommand(distutils.cmd.Command):
+    user_options = []
+    CLEAN_FILES = './build ./dist ./*.egg-info'.split()
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        here = os.path.dirname(os.path.realpath(__file__))
+
+        for path_spec in self.CLEAN_FILES:
+            abs_paths = glob.glob(os.path.normpath(os.path.join(here, path_spec)))
+            for path in [str(p) for p in abs_paths]:
+                if not path.startswith(here):
+                    raise ValueError(f"{path} is not a path inside {here}")
+                print(f'removing {os.path.relpath(path)}')
+                shutil.rmtree(path)
+
 
 setup(name='importer',
-      version='0.10',
+      version='0.10.0',
       description='Importer',
       scripts=['./runner-import.py',],
       #packages=['importer.downloaders', 'importer.loaders', 'importer.sql', 'importer.loggers', 'importer.commands'],
       packages=find_packages(exclude=("lambda",)),
       # data_files=[('./', ['importer/requirements.npi.txt'])],
       # packages=['importer'],
+      cmdclass = {
+                  'clean': CleanCommand
+                  },
       install_requires=[
         "boto3==1.7.45",
         "botocore==1.10.45",
