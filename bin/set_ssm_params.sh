caf547359d1e489c0f3e2bba959e891ef0e94559
Kyle <Kyle.Flavin@gmail.com>
2020-12-28 17:43:06 -0800

    Disable PAGER

--- bin/set_ssm_params.sh
+++ bin/set_ssm_params.sh
@@ -2,11 +2,11 @@
 
 STAGE=${1:-dev}
 
-aws ssm put-parameter --name "/importer/${STAGE}/db_host" --type "String" --value "${db_host}" --overwrite
-aws ssm put-parameter --name "/importer/${STAGE}/db_user" --type "SecureString" --value "${db_user}" --overwrite
-aws ssm put-parameter --name "/importer/${STAGE}/db_password" --type "SecureString" --value "${db_password}" --overwrite
-aws ssm put-parameter --name "/importer/${STAGE}/db_schema" --type "SecureString" --value "${db_schema}" --overwrite
-aws ssm put-parameter --name "/importer/${STAGE}/stage_db_schema" --type "SecureString" --value "${stage_db_schema}" --overwrite
+AWS_PAGER="" aws ssm put-parameter --name "/importer/${STAGE}/db_host" --type "String" --value "${db_host}" --overwrite
+AWS_PAGER="" aws ssm put-parameter --name "/importer/${STAGE}/db_user" --type "SecureString" --value "${db_user}" --overwrite
+AWS_PAGER="" aws ssm put-parameter --name "/importer/${STAGE}/db_password" --type "SecureString" --value "${db_password}" --overwrite
+AWS_PAGER="" aws ssm put-parameter --name "/importer/${STAGE}/db_schema" --type "SecureString" --value "${db_schema}" --overwrite
+AWS_PAGER="" aws ssm put-parameter --name "/importer/${STAGE}/stage_db_schema" --type "SecureString" --value "${stage_db_schema}" --overwrite
 
 
 # Must set use_replica=1 on the lambda environment variable
